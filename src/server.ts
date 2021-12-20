4f30c8d56fc3feef406f3b88bf5c8b2c0127e89e
Seb Patin <seb@Sebs-MacBook-Air.local>
2021-12-20 16:59:08 -0500

    other fix implemented

--- src/server.ts
+++ src/server.ts
@@ -134,5 +134,5 @@ app.delete("/notes/:id", async (req, res) => {
 
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode");
 });
